00084f3b7a6a2811259113f8ad59628caa61a976
areebtariq2001 <areebtaiq20@gmail.com>
2026-06-11 12:23:51 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -243,3 +243,13 @@ async def analyze_java_endpoint(file: UploadFile = File(...)):
 @app.get("/")
 def root():
     return {"message": "Legacy Migration Tool API is running!"}
+    from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
